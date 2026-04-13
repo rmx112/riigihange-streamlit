dbe975ac947c318b868e1532dcf64256dfb91ada
rmx112 <108388924+rmx112@users.noreply.github.com>
2026-04-13 14:38:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ def filter_items(items, keyword: str):
 
 
 st.set_page_config(page_title="riigihangete registri otsing", layout="wide")
-st.title("riigihangete registri märksõnaotsing")
+st.title("Riigihangete registri märksõnaotsing")
 st.write("Sisesta märksõna ja rakendus otsib selle järgi riigihangete RSS voost sobivad hanked.")
 
 keyword = st.text_input("sisesta märksõna", placeholder="näiteks: server")
